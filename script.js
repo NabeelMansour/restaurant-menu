b836731a689711dbb3415deaa7dde04dc7a4158e
verbitskayaZoya <103507783+verbitskayaZoya@users.noreply.github.com>
2024-03-22 22:08:58 +0700

    ADD Clicked Items Render in Order Section
    HTML: added order-container
    CSS: added style for order-item & order-container h2
    JS: added functions & eventListener to render clicked menu items in "Your order" section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import menuArray from "./data.js";
 
 const container = document.querySelector("#item-container");
-const order = document.querySelector("#order");
+const order = document.querySelector("#order");   
 
 const items = menuArray
   .map((item) => {
@@ -26,38 +26,88 @@ const items = menuArray
 
 container.innerHTML = items;
 
+
+// MY CODE FOR THE ORDER DISPLAY
+
+let currentItem = { } // TO STORE NAME AND PRICE
+
+const menuAdd = document.querySelectorAll(".menu-add") // IT RETURNS A NODELIST SO ADDEVENTLISTENER CAN BE USED ON IT DIRECTLY
+
+menuAdd.forEach((icon) => {                                   // I'D LIKE TO TRY & FIND A WAY TO USE EVENTLISTENER WITHOUT FOREACH       
+  icon.addEventListener("click", (e) => {                    // LIKE ON A PARENT CONTAINER BUT IT WILL REQUIRE TO REFACTOR DYNAMIC RENDERING I GUESS
+    menuArray.find((menuItem) => {
+      if(menuItem.id == e.target.dataset.add) {
+        currentItem = {
+                   name: menuItem.name,
+                   price: menuItem.price
+                      } 
+               }
+         })
+      displayOrderItems()
+})
+  })
+
+    
+//   //  FIRST I MADE IT WORK THROUGH FOR LOOP THEN I USED .FIND METHOD ABOVE
+//   // through for loop
+//   // for(let i = 0; i < menuArray.length; i++) {
+//   //   if(menuArray[i].id == e.target.dataset.add) {
+//   //       currentItem = {
+//   //                   name: menuArray[i].name,
+//   //                   price: menuArray[i].price
+//   //                     }                       
+//   //     }
+//   // }
+
+function displayOrderItems() {
+  document.getElementById("order-container-title").style.display = "block"
+  order.innerHTML += `
+              <div class="order-item">
+              <p> ${currentItem.name} </p>
+              <p> &#36;${currentItem.price} </p>
+              </div> `
+}
+
+
+// ANOTHER IDEA WAS TO USE THROUGH:
+// container.addEventListener("click", (e) => {
+//   console.log(`${e.target.parentElement.parentElement.childNodes[3].innerHTML} `)
+// })
+
+
+// ===================== NABEEL'S CODE ============================ 
 // ===== EventListeners ===== //
 
-document.addEventListener("click", function (e) {
-  if (e.target.dataset.add == 0) {
-    addPizzaToBill(e.target.dataset.add);
-  } else if (e.target.dataset.add == 1) {
-    addBurgerToBill(e.target.dataset.add);
-  } else if (e.target.dataset.add == 2) {
-    addBeerToBill(e.target.dataset.add);
-  }
-});
+// document.addEventListener("click", function(e) {
+//   if (e.target.dataset.add == 0) {
+//     addPizzaToBill(e.target.dataset.add);
+//   } else if (e.target.dataset.add == 1) {
+//     addBurgerToBill(e.target.dataset.add);
+//   } else if (e.target.dataset.add == 2) {
+//     addBeerToBill(e.target.dataset.add);
+//   }
+// });
 
-// ===== Event Functions ===== //
+// // ===== Event Functions ===== //
 
-const addPizzaToBill = () => {
-  render();
-};
+// const addPizzaToBill = () => {
+//   render();
+// };
 
-const addBurgerToBill = () => {
-  console.log("Burger");
-};
+// const addBurgerToBill = () => {
+//   console.log("Burger");
+// };
 
-const addBeerToBill = () => {
-  console.log("Beer");
-};
+// const addBeerToBill = () => {
+//   console.log("Beer");
+// };
 
-const getFoodHtml = () => {
-  let foodHtml = "";
+// const getFoodHtml = () => {
+//   let foodHtml = "";
 
-  return foodHtml;
-};
+//   return foodHtml;
+// };
 
-function render() {
-  document.getElementById("order").innerHTML = getFoodHtml();
-}
+// function render(id) {
+//   document.getElementById("order").innerHTML = getFoodHtml();
+// }
